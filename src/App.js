0c1f964e80e6797af7b3e75cf509429fb9d6736b
Juli S <strukjulia@gmail.com>
2023-03-03 21:20:24 +0100

    Add correct link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by <a hres="https://comforting-conkies-ab24d5.netlify.app/" target="_blank"> Julia Struk</a> and this 
+        This project was coded by <a href="https://comforting-conkies-ab24d5.netlify.app/" target="_blank"> Julia Struk </a> {" "} and this 
         {" "} 
       <a href="https://github.com/juliasstruk/react-weather-app" target="_blank">
          open-sourced on GitHub
