2e57595d2331aa236f6b0d0ad9140dc4a1de50f9
Juli S <strukjulia@gmail.com>
2023-03-03 21:15:35 +0100

    Adedd GitHub

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a hres="#" target="_blank"> Julia Struk</a> and this 
+        {" "} 
+      <a href="https://github.com/juliasstruk/react-weather-app" target="_blank">
+         open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
-
