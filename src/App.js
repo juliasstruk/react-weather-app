fcb376f300cafd82ece9d09c478fa372ddcfc311
Juli S <strukjulia@gmail.com>
2023-03-03 21:51:33 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+    
       <footer>
-        This project was coded by <a href="https://comforting-conkies-ab24d5.netlify.app/" target="_blank"> Julia Struk </a> {" "} and this 
+        This project was coded by <a href="https://comforting-conkies-ab24d5.netlify.app/" target="_blank" rel="noreferrer"> Julia Struk </a> {" "} and this 
         {" "} 
-      <a href="https://github.com/juliasstruk/react-weather-app" target="_blank">
+      <a href="https://github.com/juliasstruk/react-weather-app" target="_blank" rel="noreferrer">
          open-sourced on GitHub
       </a>
       </footer>
+    </div>
     </div>
   );
 }
