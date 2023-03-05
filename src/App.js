8eb80816bea761d6775d6905511f75f89cb35aba
Juli S <strukjulia@gmail.com>
2023-03-05 19:18:39 +0100

    Add Api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
 
-    <Weather />
+    <Weather defaultCity="New York"/>
       <footer>
         This project was coded by <a href="https://comforting-conkies-ab24d5.netlify.app/" target="_blank" rel="noreferrer"> Julia Struk </a> {" "} and this 
         {" "} 
